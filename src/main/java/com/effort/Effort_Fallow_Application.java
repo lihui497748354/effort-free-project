22fe984da99ddf8eb79ec80bc63c556fd2eb4e51
lihui497748354 <30363199+lihui497748354@users.noreply.github.com>
2017-09-25 20:38:14 +0800

    修改东西

--- src/main/java/com/effort/Effort_Fallow_Application.java
+++ src/main/java/com/effort/Effort_Fallow_Application.java
@@ -2,8 +2,10 @@ package com.effort;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Effort_Fallow_Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Effort_Fallow_Application.class, args);
